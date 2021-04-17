3ca85f8c845aae8bd467db42bab2211e8094038e
Alice Chen <chenalice2020@gmail.com>
2021-04-17 12:16:00 -0500

    integrated barebones lineart generator

--- app.py
+++ app.py
@@ -3,15 +3,23 @@ from flask import Flask, request, redirect, send_file, url_for
 from werkzeug.utils import secure_filename
 from flask_cors import CORS #comment this on deployment
 
-UPLOAD_FOLDER = "./testuploads"
+import sys
+sys.path.insert(1, './lineart-model/src/')
+
+import genlineart
+
+UPLOAD_FOLDER = "./lineart-model/images"
+DOWNLOAD_FOLDER = "./lineart-model/outputimages"
 
 app = Flask(__name__)
 CORS(app) #comment this on deployment
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 
 @app.route('/img/<filename>', methods = ['GET'])
 def give(filename):
-    filen = './testuploads/'+ filename
+    filen = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     return send_file(filen)
 
 @app.route('/upload', methods=['POST'])
