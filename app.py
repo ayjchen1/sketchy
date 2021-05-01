6cf075db3b18b6db6f0e59d6f55e99cf93945174
Alice Chen <chenalice2020@gmail.com>
2021-05-01 12:44:03 -0500

    cycleGAN

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
-from flask import Flask, request, redirect, send_file, url_for
+from flask import Flask, request, redirect, send_file, url_for, after_this_request
 from werkzeug.utils import secure_filename
 from flask_cors import CORS #comment this on deployment
 
 import sys
-sys.path.insert(1, './lineart-model/src/')
-import genlineart
+sys.path.insert(1, './cycleGAN/')
+import inference
 
-UPLOAD_FOLDER = "./lineart-model/images"
-DOWNLOAD_FOLDER = "./lineart-model/outputimages"
+UPLOAD_FOLDER = "./cycleGAN/inputs/test/B"
+DOWNLOAD_FOLDER = "./cycleGAN/output/A"
 
 app = Flask(__name__)
 CORS(app) #comment this on deployment
@@ -20,13 +20,7 @@ app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 def give(filename):
     filen = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
     print(filen)
-    return send_file(filen)
-
-def create_lineart(filename):
-    inputpath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    outputpath = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
-
-    genlineart.generate(inputpath, outputpath)
+    return send_file(filen)    
 
 @app.route('/upload', methods=['POST'])
 def image_upload():
@@ -37,10 +31,18 @@ def image_upload():
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
         
-        create_lineart(filename)
+        inference.perform_inference(dataroot="./cycleGAN/inputs/", A2B=False, B2A=True,\
+        generator_A2B="./cycleGAN/output/netG_A2B.pth", generator_B2A="./cycleGAN/output/netG_B2A.pth", cuda=False,
+        filename=filename)
 
         apiURL = "/img/" + filename
         print(apiURL)
+
+        @after_this_request 
+        def remove_file(response): 
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return response
+
         return {'fileurl': apiURL}
 
     return {'fileurl': ""}
